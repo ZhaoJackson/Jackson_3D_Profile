e46d5a10975e73a3d48ae3efbcdc85b2b5e9e9e2
Jackson Zhao <zz3119@columbia.edu>
2026-05-04 15:35:22 -0400

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    build: {
-        chunkSizeWarningLimit: 100000000
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ["legacy-js-api", "import"],
+      },
     },
-    base: process.env.NODE_ENV === 'production' ? "/Jackson_3D_Profile/" : "/",
+  },
+  build: {
+    chunkSizeWarningLimit: 100000000,
+  },
+  base: process.env.NODE_ENV === 'production' ? "/Jackson_3D_Profile/" : "/",
 })
