a80bc805ed1ea0bdb3a935fdcd4256b859b0c0b3
Jackson Zhao <zz3119@columbia.edu>
2026-05-07 21:12:21 -0400

    commit

--- src/data/education.js
+++ src/data/education.js
@@ -1,14 +1,14 @@
 import { nyu, bits } from "../assets";
 const educations = [
   {
-    degree: "Master",
+    degree: "Master of Arts",
     branch: "Research Methodology & Quantitative Methods",
     name: "Columbia University",
     year: "2023 - 2024",
     image: nyu,
   },
   {
-    degree: "Bachelor",
+    degree: "Bachelor of Arts",
     branch: "Psychology | Mathematics",
     name: "DePauw University",
     year: "2018 - 2023",
